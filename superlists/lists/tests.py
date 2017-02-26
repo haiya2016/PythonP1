b024b18bc7a0afa94beec155c5dfd92d2c4fa8ae
weijiaxin <250214445@qq.com>
2017-02-27 00:24:05 +0800

    app test past

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
 
@@ -8,4 +10,9 @@ class HomePageTest(TestCase):
 		found = resolve('/')    #resolve 用于从根路径“/“查找函数
 		self.assertEqual(found.func, home_page)
 
-		
+	def test_home_page_retruns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))  #断言响应的content属性以html标签开头
+		self.assertIn(b'<title>To-Do list<title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))		
--- tdd.py
+++ tdd.py
@@ -1,21 +1,42 @@
 #coding = utf-8
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.brower = webdriver.Chrome()
-		self.brower.implicitly_wait(3)
+		self.brower.implicitly_wait(10)
 
 	def tearDown(self):
 		self.brower.quit()
 
-	def test_can_it_work(self):
+	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.brower.get('http://localhost:8000')
 
 		self.assertIn('To-Do', self.brower.title)
+		header_text = self.brower.find_element_by_tag_name('h1').text
 		self.fail("Finish the test")
+
+		#应用可以输入一个待办事项
+		inputbox = self.brower.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
+
+		#文本框中输入了“buy a car"
+		inputbox.send_keys('buy a car')
+
+		#按下回车键，返回显示"1：buy a car"
+		inputbox.send_keys(keys.ENTER)
+
+		table = self.brower.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: buy a car' for row in rows)
+			)
 
 if __name__ == '__main__':
 	unittest.main()
