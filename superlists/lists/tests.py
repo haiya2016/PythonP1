300dc536f17b1d0e984087d2115902301309e5c5
weijiaxin <250214445@qq.com>
2017-03-12 11:37:05 +0800

    csrf_token is a difficult promblem

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -10,9 +11,31 @@ class HomePageTest(TestCase):
 		found = resolve('/')    #resolve 用于从根路径“/“查找函数
 		self.assertEqual(found.func, home_page)
 
-	def test_home_page_retruns_correct_html(self):
+	# def test_home_page_retruns_correct_html(self):
+	# 	request = HttpRequest()
+	# 	response = home_page(request)
+	# 	# self.assertTrue(response.content.startswith(b'<html>'))  #断言响应的content属性以html标签开头
+	# 	# self.assertIn(b'<title>To-Do lists</title>', response.content)
+	# 	# self.assertTrue(response.content.endswith(b'</html>'))
+	# 	expected_html = render_to_string('home.html')
+	# 	print(expected_html)
+	# 	print(response.content.decode())
+	# 	print(response.content)
+
+	# 	self.assertEqual(response.content.decode(), expected_html)	
+
+	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))  #断言响应的content属性以html标签开头
-		self.assertIn(b'<title>To-Do list<title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))		
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+			)
+		print(expected_html)
+		print(response.content.decode())
+		self.assertEqual(response.content.decode(), expected_html)
+
--- superlists/tdd.py
+++ superlists/tdd.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -12,12 +13,17 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.brower.quit()
 
+	def check_for_rows_in_list_table(self, row_text):
+		table = self.brower.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.brower.get('http://localhost:8000')
 
 		self.assertIn('To-Do', self.brower.title)
 		header_text = self.brower.find_element_by_tag_name('h1').text
-		self.fail("Finish the test")
+		
 
 		#应用可以输入一个待办事项
 		inputbox = self.brower.find_element_by_id('id_new_item')
@@ -25,18 +31,19 @@ class NewVisitorTest(unittest.TestCase):
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 			)
-
 		#文本框中输入了“buy a car"
 		inputbox.send_keys('buy a car')
-
 		#按下回车键，返回显示"1：buy a car"
-		inputbox.send_keys(keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
+		# time.sleep(10)
 
-		table = self.brower.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: buy a car' for row in rows)
-			)
+		inputbox = self.brower.find_element_by_id('id_new_item')
+		inputbox.send_keys('buy a computer')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.check_for_rows_in_list_table('1: buy a car')
+		self.check_for_rows_in_list_table('2: buy a caomputer')
+		self.fail("Finish the test")
 
 if __name__ == '__main__':
 	unittest.main()
